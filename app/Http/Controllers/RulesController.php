7ac909794282c4f60dd84daf7e58feed97fe4bce
mhdtaufikrm <mtaufikramadhan27@gmail.com>
2023-07-07 17:20:53 +0700

    done rules dropdown and user

---
+++ app/Http/Controllers/RulesController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Rule;
+
+use Illuminate\Http\Request;
+
+class RulesController extends Controller
+{
+    public function index()
+    {
+        $rules = Rule::get();
+        return view('rules.index',compact('rules'));
+    }
+
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'rule_name' => 'required',
+            'rule_value' => 'required',
+        ]);
+
+        $addrule=Rule::create([
+            'rule_name' => $request->rule_name,
+            'rule_value' => $request->rule_value,
+        ]);
+        if ($addrule) {
+            return redirect('/rule')->with('status','Success Add Rule');
+        }else{
+            return redirect('/rule')->with('status','Failed Add Rule');
+        }
+    }
+
+    public function delete($id)
+    {
+        $deleterule=Rule::where('id',$id)
+        ->delete();
+        if ($deleterule) {
+            return redirect('/rule')->with('status','Success Delete Rule');
+        }else{
+            return redirect('/rule')->with('status','Failed Delete Rule');
+        }
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'rule_name' => 'required',
+            'rule_value' => 'required',
+        ]);
+
+        $updaterule=Rule::where('id',$id)
+        ->update([
+            'rule_name' => $request->rule_name,
+            'rule_value' => $request->rule_value,
+        ]);
+        if ($updaterule) {
+            return redirect('/rule')->with('status','Success Update Rule');
+        }else{
+            return redirect('/rule')->with('status','Failed Update Rule');
+        }
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\DropdownController;
+use App\Http\Controllers\RulesController;
+use App\Http\Controllers\UserController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +18,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Home Controller
+Route::get('/home', [HomeController::class, 'index']);
+
+//Dropdown Controller
+Route::get('/dropdown', [DropdownController::class, 'index']);
+Route::post('/dropdown/store', [DropdownController::class, 'store']);
+Route::patch('/dropdown/update/{id}', [DropdownController::class, 'update']);
+Route::delete('/dropdown/delete/{id}', [DropdownController::class, 'delete']);
+
+//Rules Controller
+Route::get('/rule', [RulesController::class, 'index']);
+Route::post('/rule/store', [RulesController::class, 'store']);
+Route::patch('/rule/update/{id}', [RulesController::class, 'update']);
+Route::delete('/rule/delete/{id}', [RulesController::class, 'delete']);
+
+//User Controller
+Route::get('/user', [UserController::class, 'index']);
+Route::post('/user/store', [UserController::class, 'store']);
+Route::patch('/user/update/{user}',[UserController::class, 'update']);
+Route::get('/user/revoke/{user}',[UserController::class, 'revoke']);
+Route::get('/user/access/{user}',[UserController::class, 'access']);
